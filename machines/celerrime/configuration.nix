f4d77440fb153d2f38bf3c0852fedeaec5392d10
Ivan Bushchik <ivabus@ivabus.dev>
2023-07-30 10:27:10 +0300

    Cleanup celerrime config
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- machines/celerrime/configuration.nix
+++ machines/celerrime/configuration.nix
@@ -3,7 +3,7 @@
 {
   imports = [  ];
   networking.hostName = "celerrime";
-  # Use the systemd-boot EFI boot loader.
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
 }
