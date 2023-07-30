f2d2801a5f66bf6555d66584dd33e6c7609caa88
Ivan Bushchik <ivabus@ivabus.dev>
2023-07-30 09:23:12 +0300

    Fix celerrime configuration using configuration from celerrime
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- machines/celerrime/configuration.nix
+++ machines/celerrime/configuration.nix
@@ -1,13 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-    # Include the necessary packages and configuration for Apple Silicon support.
-    <apple-silicon-support/apple-silicon-support>
-  ];
-
+  imports = [  ];
+  networking.hostName = "celerrime";
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
