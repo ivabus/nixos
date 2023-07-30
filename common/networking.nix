b78065a71e237018ba3da9e8c86afd00f0410d44
Ivan Bushchik <ivabus@ivabus.dev>
2023-07-30 16:47:22 +0300

    Add avahi + enable IPv6
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- common/networking.nix
+++ common/networking.nix
@@ -10,6 +10,13 @@
 
   networking.nameservers = [ "1.1.1.1" "1.0.0.1" "8.8.8.8" ];
 
+  networking.enableIPv6 = true;
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
+
   services.timesyncd.enable = true;
   networking.timeServers = [ "ntp1.vniiftri.ru" "0.ru.pool.ntp.org" "0.pool.ntp.org" ];
 }
