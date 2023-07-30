4b9a9bc6a7d39429f2c3bb59585d209c9ee9407f
Ivan Bushchik <ivabus@ivabus.dev>
2023-07-30 16:59:19 +0300

    Fix avahi conf
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- common/networking.nix
+++ common/networking.nix
@@ -12,9 +12,18 @@
 
   networking.enableIPv6 = true;
 
+  services.resolved.enable = true;
   services.avahi = {
     enable = true;
     nssmdns = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
   };
 
   services.timesyncd.enable = true;
