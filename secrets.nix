e4fb67d8ebbe15d671a80882a93161843aac0f96
Ivan Bushchik <ivabus@ivabus.dev>
2023-10-03 22:14:50 +0300

    secrets.nix: Edit secrets abort message
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- secrets.nix
+++ secrets.nix
@@ -4,7 +4,7 @@ let
   expectedHash =
     "bc6f38a927602241c5e0996b61ebd3a90d5356ca76dc968ec14df3cd45c6612c";
 in if (canaryHash != expectedHash && config.my.features.secrets) then
-  abort "Secrets are not readable. Have you run `git-crypt unlock`?"
+  abort "Secrets are enabled and not readable. Have you run `git-crypt unlock`?"
 else {
   hashed-password = builtins.readFile ./secrets/hashed-password;
 }
