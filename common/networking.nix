c0cc93bdb1ecaa21ef43438d1b5369a2bd693196
Ivan Bushchik <ivabus@ivabus.dev>
2023-12-22 07:29:59 +0300

    Disable unnecessary avahi config
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- common/networking.nix
+++ common/networking.nix
@@ -23,15 +23,6 @@
 
   services.avahi = {
     enable = true;
-    nssmdns = true;
-    publish = {
-      enable = true;
-      addresses = true;
-      domain = true;
-      hinfo = true;
-      userServices = true;
-      workstation = true;
-    };
   };
 
   services.chrony.enable = true;
