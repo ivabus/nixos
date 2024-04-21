f4990de0831914fc5ec09aa937f8b2d0badae814
Ivan Bushchik <ivabus@ivabus.dev>
2024-04-21 10:50:23 +0300

    Bump urouter to 0.7.0
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- pkgs/urouter.nix
+++ pkgs/urouter.nix
@@ -3,14 +3,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "urouter";
-  version = "0.6.1";
+  version = "0.7.0";
 
   src = fetchCrate {
     inherit pname version;
-    sha256 = "sha256-1HKAqX15gvfzUFpyt0FFq9NZog1XSgeEYCrGVu4lSKM=";
+    sha256 = "sha256-2fPrZ4jn9bt7VwSnBXW65sTcrb+pcpMgexpGnz2n12w=";
   };
 
-  cargoSha256 = "sha256-e/gVAmLhHhkPqxanTSz/qrayJUlBcFT0/WjA726H0bk=";
+  cargoSha256 = "sha256-8bOX/aFSLWqLdjqpZA67iqaJMIQRMPRK38gbI9//tD8=";
 
   nativeBuildInputs = [ pkgs.pkg-config ];
 }
