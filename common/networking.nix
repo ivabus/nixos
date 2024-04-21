5695e7b0a58da04f6a02798c6fd9185805e9575a
Ivan Bushchik <ivabus@ivabus.dev>
2024-04-21 10:52:14 +0300

    Add IPs to timeServers and remove DoT
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- common/networking.nix
+++ common/networking.nix
@@ -6,18 +6,7 @@
   networking.useNetworkd = lib.mkDefault true;
   systemd.network.wait-online.enable = lib.mkDefault false;
 
-  # Use systemd-resolved for DoT support.
-  services.resolved = {
-    enable = true;
-    dnssec = "false";
-    extraConfig = ''
-      DNSOverTLS=yes
-    '';
-  };
-
-  # Used by systemd-resolved, not directly by resolv.conf.
-  networking.nameservers =
-    [ "1.0.0.1#cloudflare-dns.com" "8.8.8.8#dns.google" ];
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" "8.8.8.8" ];
 
   networking.enableIPv6 = true;
 
@@ -34,7 +23,7 @@
 
   services.chrony.enable = true;
   networking.timeServers =
-    [ "ntp1.vniiftri.ru" "0.ru.pool.ntp.org" "0.pool.ntp.org" ];
+    [ "89.109.251.21" "89.109.251.22" "ntp1.vniiftri.ru" "0.ru.pool.ntp.org" "0.pool.ntp.org" ];
 
   # Useful tools
   boot.kernelModules = [ "af_packet" ];
